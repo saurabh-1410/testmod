d96754ba4478634eac7b4697ce1f5fb6539df88e
Saurabh D <saurabh.dikshit@puppet.com>
2024-04-02 16:43:02 +0100

    add

--- manifests/win.pp
+++ manifests/win.pp
@@ -12,7 +12,7 @@ define testmod::win (
 
   exec { 'join_domain':
         environment => [ "Password=abcd1234" ],
-        command   => 'exit 0',
+        command   => 'exit 1',
         unless   => 'if((Get-WmiObject -Class Win32_ComputerSystem).domain -ne ${domain}){exit 1}',
         provider  => powershell,
        }
