c4b529834666e0916bdbef7798c99f63336e5e2e
Saurabh D <saurabh.dikshit@puppet.com>
2024-04-05 11:45:11 +0100

    foo

--- manifests/win.pp
+++ manifests/win.pp
@@ -3,7 +3,7 @@ class testmod::win (
  )
   {
 
-
+tag 'foo' 'bar'
   exec { 'create_file':
         command     => 'New-Item -Path "C:\Users\saurabh_dikshit\Desktop\file1" -ItemType "file" -Force',
         provider    => powershell,
