e6a47c25047f87e8ece679cba1610001a2a7dd30
Saurabh D <saurabh.dikshit@puppet.com>
2025-01-02 19:14:07 +0530

    pdk

--- manifests/init.pp
+++ manifests/init.pp
@@ -23,4 +23,3 @@ class testmod (
     target       => $target,
   }
 }
-
--- manifests/win.pp
+++ manifests/win.pp
@@ -1,21 +1,19 @@
 class testmod::win (
-  Boolean $noop = $clientnoop 
- )
-  {
-
+  Boolean $noop = $clientnoop
+) {
   exec { 'create_file':
-        command     => 'New-Item -Path "C:\Users\saurabh_dikshit\Desktop\file1" -ItemType "file" -Force',
-        provider    => powershell,
-        logoutput   => true,
-        noop        => $noop,
-        tag         => 'foo'
-    }
+    command   => 'New-Item -Path "C:\Users\saurabh_dikshit\Desktop\file1" -ItemType "file" -Force',
+    provider  => powershell,
+    logoutput => true,
+    noop      => $noop,
+    tag       => 'foo',
+  }
 
   exec { 'join_domain':
-        environment => [ "Password=abcd1234" ],
-        command   => 'cat /etc/profile',
-        unless   => 'if((Get-WmiObject -Class Win32_ComputerSystem).domain -ne ${domain}){exit 1}',
-        provider  => powershell,
-        noop      => $noop
-       }
+    environment => ['Password=abcd1234'],
+    command     => 'cat /etc/profile',
+    unless      => "if((Get-WmiObject -Class Win32_ComputerSystem).domain -ne ${domain}){exit 1}",
+    provider    => powershell,
+    noop        => $noop,
   }
+}
