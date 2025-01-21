cfda568123fdfad47367296e9e3a9fb549762ea1
Saurabh D <saurabh.dikshit@puppet.com>
2025-01-21 15:59:41 +0530

    sd

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,16 +1,5 @@
 #class testmod {
 
-#file { '/tmp/file1':
-#  ensure  => present,
-#  content => 'Hello testmod',
-#  noop    => false,
-#}
-#
-#service { 'chronyd.service' :
-#  ensure => stopped,
-#  enable => true,
-#}
-
 class testmod (
   Array  $host_aliases  = [],
   String $ip,
@@ -21,5 +10,16 @@ class testmod (
     host_aliases => $host_aliases,
     ip           => $ip,
     target       => $target,
+  }
+
+  file { '/tmp/file1':
+    ensure  => file,
+    content => 'Hello testmod',
+    noop    => false,
+  }
+
+  service { 'chronyd.service' :
+    ensure => stopped,
+    enable => true,
   }
 }
