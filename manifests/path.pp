1d8f5f0bc1e3bdc05cbf04d02dddf1ead67f43d3
Saurabh D <saurabh.dikshit@puppet.com>
2025-01-22 17:22:50 +0530

    as

--- manifests/path.pp
+++ manifests/path.pp
@@ -8,5 +8,8 @@ class testmod::path {
 exec { 'show_path':
   command => '/usr/bin/echo $PATH > /tmp/puppet_path',
 }
-
+exec { " toto create_mount_point":
+command => "touch /opt/hello",
+unless => "test -d /opt",
+}
 }
